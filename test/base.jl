2a79179c03380392aa4407f47132e07087c3e732
Long Wang <wangl.cc@outlook.com>
2021-05-27 20:44:34 +0800

    test: more test for DynamicRScalar

--- test/base.jl
+++ test/base.jl
@@ -2,17 +2,19 @@ using RecordedArrays: rsize, rlength, StaticEntries, DynamicEntries
 
 # init test vars
 c = DiscreteClock(1)
-DS, DV1, DV2 = DynamicRArray(c, 1, [1], [1, 2])
+DS1, DS2, DV1, DV2 = DynamicRArray(c, 1, fill(1), [1], [1, 2])
 SV1, SV2 = StaticRArray(c, [1], [1, 2])
 
 @testset "length and size" begin
-    @test length(DS)  == 1
+    @test length(DS1)  == 1
+    @test length(DS2)  == 1
     @test length(DV1) == 1
     @test length(DV2) == 2
     @test length(SV1) == 1
     @test length(SV2) == 2
 
-    @test size(DS)  == (1,)
+    @test size(DS1)  == (1,)
+    @test size(DS2)  == (1,)
     @test size(DV1) == (1,)
     @test size(DV2) == (2,)
     @test size(SV1) == (1,)
@@ -21,6 +23,8 @@ end
 
 # push! and delateat!
 for _ in c
+    DS1[1] += 1
+    DS2[1] += 1
     push!(SV1, 2)
     push!(DV1, 2)
     DV1[1] += 1
@@ -29,13 +33,15 @@ for _ in c
 end
 
 @testset "length and size after change" begin
-    @test length(DS)  == 1
+    @test length(DS1) == 1
+    @test length(DS2) == 1
     @test length(DV1) == 2
     @test length(DV2) == 1
     @test length(SV1) == 2
     @test length(SV2) == 1
 
-    @test size(DS)  == (1,)
+    @test size(DS1)  == (1,)
+    @test size(DS2)  == (1,)
     @test size(DV1) == (2,)
     @test size(DV2) == (1,)
     @test size(SV1) == (2,)
@@ -43,13 +49,15 @@ end
 end
 
 @testset "rlength and rsize" begin
-    @test rlength(DS)  == 1
+    @test rlength(DS1) == 1
+    @test rlength(DS2) == 1
     @test rlength(DV1) == 2
     @test rlength(DV2) == 2
     @test rlength(SV1) == 2
     @test rlength(SV2) == 2
 
-    @test rsize(DS)  == (1,)
+    @test rsize(DS1) == (1,)
+    @test rsize(DS2) == (1,)
     @test rsize(DV1) == (2,)
     @test rsize(DV2) == (2,)
     @test rsize(SV1) == (2,)
@@ -59,22 +67,26 @@ end
 # get records
 Dr1 = records(DV1)
 Dr2 = records(DV2)
+Dr3 = records(DS1)
 Sr1 = records(SV1)
 Sr2 = records(SV2)
 
 @testset "Records" begin
     @test Base.IteratorSize(typeof(Dr1)) == Base.HasShape{1}()
     @test Base.IteratorSize(typeof(Dr2)) == Base.HasShape{1}()
+    @test Base.IteratorSize(typeof(Dr3)) == Base.HasShape{0}()
     @test Base.IteratorSize(typeof(Sr1)) == Base.HasShape{1}()
     @test Base.IteratorSize(typeof(Sr2)) == Base.HasShape{1}()
 
     @test eltype(typeof(Dr1)) == DynamicEntries{Int,Int}
     @test eltype(typeof(Dr2)) == DynamicEntries{Int,Int}
+    @test eltype(typeof(Dr3)) == DynamicEntries{Int,Int}
     @test eltype(typeof(Sr1)) == StaticEntries{Int,Int}
     @test eltype(typeof(Sr2)) == StaticEntries{Int,Int}
 
     @test length(Dr1) == 2
     @test length(Dr2) == 2
+    @test length(Dr3) == 1
     @test length(Sr1) == 2
     @test length(Sr2) == 2
 end
@@ -83,6 +95,7 @@ end
 e1 = Dr1[1]
 e2 = Dr1[2]
 e3 = Sr2[1]
+e4 = Dr3[1]
 u1 = unione(e1)
 u12 = unione(e1, e2)
 u23 = unione(e2, e3)
@@ -95,6 +108,7 @@ u1223 = unione(u12, u23)
     @test eltype(e1) == Pair{Int,Int}
     @test eltype(e2) == Pair{Int,Int}
     @test eltype(e3) == Pair{Int,Int}
+    @test eltype(e4) == Pair{Int,Int}
     @test eltype(u1) == Pair{Int,Vector{Int}}
     @test eltype(u12) == Pair{Int,Vector{Int}}
     @test eltype(u23) == Pair{Int,Vector{Int}}
@@ -106,6 +120,7 @@ u1223 = unione(u12, u23)
     @test length(e1) == 2
     @test length(e2) == 1
     @test length(e3) == 2
+    @test length(e4) == 2
     @test length(u1) == 2
     @test length(u12) == 2
     @test length(u23) == 2
@@ -121,6 +136,9 @@ u1223 = unione(u12, u23)
 
     @test getindex(e3, 1) == (0 => 1)
     @test getindex(e3, 2) == (1 => 1)
+
+    @test getindex(e4, 1) == (0 => 1)
+    @test getindex(e4, 2) == (1 => 2)
 
     @test getindex(u1, 1) == (0 => [1])
     @test getindex(u1, 2) == (1 => [2])
@@ -148,6 +166,7 @@ end
     @test gettime(e1, 0) == 1
     @test gettime(e2, 0) == 0
     @test gettime(e3, 0) == 1
+    @test gettime(e4, 0) == 1
     @test gettime(u1, 0) == [1]
     @test gettime(u12, 0) == [1, 0]
     @test gettime(u23, 0) == [0, 1]
@@ -159,6 +178,7 @@ end
     @test gettime(e1, 1) == 2
     @test gettime(e2, 1) == 2
     @test gettime(e3, 1) == 1
+    @test gettime(e4, 1) == 2
     @test gettime(u1, 1) == [2]
     @test gettime(u12, 1) == [2, 2]
     @test gettime(u23, 1) == [2, 1]
@@ -170,6 +190,7 @@ end
     @test gettime(e1, 2) == 2
     @test gettime(e2, 2) == 2
     @test gettime(e3, 2) == 0
+    @test gettime(e4, 2) == 2
     @test gettime(u1, 2) == [2]
     @test gettime(u12, 2) == [2, 2]
     @test gettime(u23, 2) == [2, 0]
