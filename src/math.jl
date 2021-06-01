00519a97c194ed8fa96034beec0ffe6da2eb3073
Long Wang <wangl.cc@outlook.com>
2021-06-01 19:32:11 +0800

    refactor: inline math

--- src/math.jl
+++ src/math.jl
@@ -21,11 +21,11 @@ end
 
 # fix for Adjoint or Transpose (copy from LinearAlgebra adjtrans.jl)
 # Adjoint/Transpose-vector * vector
-*(u::AdjointAbsVec{<:Number}, v::AbstractRVector{<:Number,<:Real}) = *(u, state(v))
-*(u::TransposeAbsVec{T}, v::AbstractRVector{T,<:Real}) where {T<:Real} = *(u, state(v))
+@inline *(u::AdjointAbsVec{<:Number}, v::AbstractRVector{<:Number,<:Real}) = *(u, state(v))
+@inline *(u::TransposeAbsVec{T}, v::AbstractRVector{T,<:Real}) where {T<:Real} = *(u, state(v))
 # vector * Adjoint/Transpose-vector
-*(u::AbstractRVector, v::AdjOrTransAbsVec) = *(state(u), v)
+@inline *(u::AbstractRVector, v::AdjOrTransAbsVec) = *(state(u), v)
 
 ## broadcast
-broadcastable(r::AbstractRArray) = state(r)
+@inline broadcastable(r::AbstractRArray) = state(r)
 # vim:tw=92:ts=4:sw=4:et
