0f1a19ad8c5df7f7c21a9ce6d96b3a7f17057ad4
Long Wang <wangl.cc@outlook.com>
2021-10-30 14:39:42 +0800

    test: remove doc test and benchmark

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,7 @@
 using Test
 using RecordedArrays
-using Documenter
-using BenchmarkTools
-
-# don't work now
-filters = Regex[
-    r"Int32|Int64",                 # Int64 <-> Int32
-    r"Array{\w+,\s?1}|Vector{\w+}", # Array{X,1} <-> Vector{X}
-    r"Array{\w+,\s?2}|Matrix{\w+}", # Array{X,2} <-> Matrix{X}
-]
-
-DocMeta.setdocmeta!(RecordedArrays, :DocTestSetup, :(using RecordedArrays); recursive=true)
 
 @testset "RecordedArrays" begin
-    if VERSION >= v"1.6" && Int64 == Int # run doctest only for v1.6+ and x64
-        doctest(RecordedArrays; testset="Doctests", doctestfilters=filters)
-
-        @testset "show" begin
-            include("show.jl")
-        end
-    end
-
     @testset "Utilities" begin
         include("utils.jl")
     end
@@ -31,11 +12,7 @@ DocMeta.setdocmeta!(RecordedArrays, :DocTestSetup, :(using RecordedArrays); recu
 
     @testset "Base" begin
         include("base.jl")
-    end
 
-    @testset "Math" begin
-        include("math.jl")
-    end
 
     @testset "Resize" begin
         include("resize.jl")
